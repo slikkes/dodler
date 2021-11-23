e22d3be918fed0afa7b10f3d6587656934a263d6
slikk <slikkistvan@gmail.com>
2021-11-23 21:53:18 +0100

    recursive element creation

--- dodler.js
+++ dodler.js
@@ -1,67 +1,77 @@
 (function(){
 	let elements = [
 	{
-		tagName: 'button',
+		tagName: 'div',
 		args:{
-			innerText: 'xxXxXxx',
-			className: 'dodleBtn',
-			style:{top: '50vh'},
-			onclick: ()=>{alert("xXxXxXXXxXxxxxXXx")}
-		}
-	},
-	{
-		tagName: 'button',
-		args:{
-			innerText: 'ooOoOoo',
-			className: 'dodleBtn',
-			style:{top: '40vh'},
-			onclick: ()=>{alert("oOoOooOooOoooOOooOo")}
-		}
-	},
-	{
-		tagName: 'button',
-		args:{
-			innerText: 'no',
-			className: 'dodleBtn',
-			style:{top: '30vh'},
-			onclick: ()=>{dodler.action = false}
-		}
-	},
-	{
-		tagName: 'button',
-		args:{
-			innerText: 'yes',
-			className: 'dodleBtn',
-			style:{top: '20vh'},
-			onclick: ()=>{dodler.action = 'yes'}
-		}
-	},
+			style: {position: 'fixed', top: '50vh', left: '16px', 'z-index': 999, display: 'flex', 'flex-direction': 'column'},
+			elements:[
+			{
+				tagName: 'button',
+				args:{
+					innerText: 'xxXxXxx',
+					className: 'dodleBtn',
+					onclick: ()=>{alert("xXxXxXXXxXxxxxXXx")}
+				}
+			},
+			{
+				tagName: 'button',
+				args:{
+					innerText: 'ooOoOoo',
+					className: 'dodleBtn',
+					onclick: ()=>{alert("oOoOooOooOoooOOooOo")}
+				}
+			},
+			{
+				tagName: 'button',
+				args:{
+					innerText: 'no',
+					className: 'dodleBtn',
+					onclick: ()=>{dodler.action = false}
+				}
+			},
+			{
+				tagName: 'button',
+				args:{
+					innerText: 'yes',
+					className: 'dodleBtn',
+					onclick: ()=>{dodler.action = 'yes'}
+				}
+			},
+			]
+		},
+
+	}
 	]
 
 	window.dodler = {
 		btns: null,
 		action: false,
 		start(conf){
-			this._addStyleSheet();
-			this.btns = conf.map(item => this._createElement(item.tagName, item.args));
-			this._setListeners();
-		},
-		_createElement(tagName, args){
-			let el = document.createElement(tagName)
-			Object.keys(args).forEach(key=>{
-				if(key === 'style'){
-					Object.keys(args.style).forEach(styleArg=>el.style[styleArg] = args.style[styleArg])
-					return
-				}
-				el[key] = args[key]
+		this._addStyleSheet();
+		this.btns = conf.map(item => this._createElement(item.tagName, item.args, document.body));
+		this._setListeners();
+	},
+	_createElement(tagName, args, parent){
+		let el = document.createElement(tagName)
+		Object.keys(args).forEach(key=>{
+			if(key === 'style'){
+				Object.keys(args.style).forEach(styleArg=>el.style[styleArg] = args.style[styleArg])
+				return
+			}
+			el[key] = args[key]
+		})
+		parent.appendChild(el)
+		if(el.elements){
+			el.elements.forEach(ch => {
+				this._createElement(ch.tagName, ch.args, el)
 			})
-			document.body.appendChild(el)
-			return el;
-		},
-		_addStyleSheet(){
-			var style = document.createElement('style');
-			style.type = 'text/css';
-			style.innerHTML = '.dodleBtn {position: fixed; left: 8px; top: 40vh; z-index: 999; }';
+		}
+		return el;
+	},
+	_addStyleSheet(){
+		var style = document.createElement('style');
+		style.type = 'text/css';
+			style.innerHTML = '.dodleBtn {padding: 4px 6px; font-weight. 700 }';
 			document.getElementsByTagName('head')[0].appendChild(style);
 		},
 		_setListeners(){
